db2228306db57ea6c7944de22aa95032a57cc22e
Vanye <37693166+vanye007@users.noreply.github.com>
2020-04-23 18:20:57 -0700

    fixing styles

--- resources/views/external_broker/index.blade.php
+++ resources/views/external_broker/index.blade.php
@@ -5,9 +5,9 @@
   <meta charset="utf-8">
   <title>Buyer Input Form</title>
   <meta content="width=device-width, initial-scale=1" name="viewport">
-  <link href="css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="css/components.css" rel="stylesheet" type="text/css">
-  <link href="css/buyer-input-form.css" rel="stylesheet" type="text/css">
+  <link href="{{asset('css/normalize.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('css/components.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('css/buyer-input-form.css')}}" rel="stylesheet" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
   <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic"]  }});</script>
   <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
@@ -58,7 +58,7 @@
     </div>
   </div>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js?site=5e8e3bd015febce0cfd0e544" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script src="js/buyer-input-form-b30044.js" type="text/javascript"></script>
+  <script src="{{asset('js/buyer-input-form-b30044.js')}}" type="text/javascript"></script>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 </body>
 </html>
